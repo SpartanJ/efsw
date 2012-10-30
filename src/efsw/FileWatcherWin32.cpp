88925533735dcf638fd3ba09716b3c642cfdc62f
Administrator@spartanvmxp <Administrator@spartanvmxp>
2012-10-30 13:12:03 -0300

    Fixed Win32 not showing directory add/delete events.

--- src/efsw/FileWatcherWin32.cpp
+++ src/efsw/FileWatcherWin32.cpp
@@ -172,7 +172,7 @@ WatchID FileWatcherWin32::addWatch(const std::string& directory, FileWatchListen
 	WatchID watchid = ++mLastWatchID;
 
 	WatcherStructWin32* watch = CreateWatch(directory.c_str(), recursive,
-		FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_FILE_NAME);
+		FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME);
 
 	if(!watch)
 		return Errors::Log::createLastError( Errors::FileNotFound, directory );
