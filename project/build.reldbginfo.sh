01b312c3ebbc6b39b884c07c51710e546cb8a617
Martín Lucas Golini <spartanj@gmail.com>
2017-10-01 02:09:54 -0300

    Force lib symlinks on build.reldbginfo.sh.

--- project/build.reldbginfo.sh
+++ project/build.reldbginfo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd ../../lib
-ln -s libefsw.so.$1.$2.$3 libefsw.so.$1
-ln -s libefsw.so.$1 libefsw.so
+ln -fs libefsw.so.$1.$2.$3 libefsw.so.$1
+ln -fs libefsw.so.$1 libefsw.so
 
 if [ "$4" == "strip-symbols" ]; then
 	objcopy --only-keep-debug libefsw.so.$1.$2.$3 libefsw.debug
