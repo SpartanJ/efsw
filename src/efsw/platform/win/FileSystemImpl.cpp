0716758f7f5d204f084c115ac7d4be58f438c521
Martín Lucas Golini <spartanj@gmail.com>
2018-11-23 18:11:54 -0300

    Linux: In case now checks if the FUSE file system is NTFS and allows to work with inotify ( since it just works ).

--- src/efsw/platform/win/FileSystemImpl.cpp
+++ src/efsw/platform/win/FileSystemImpl.cpp
@@ -13,6 +13,38 @@
 
 namespace efsw { namespace Platform {
 
+bool FileSystem::changeWorkingDirectory( const std::string & path )
+{
+	int res;
+#ifdef EFSW_COMPILER_MSVC
+	#ifdef UNICODE
+	res = _wchdir( String::fromUtf8( path.c_str() ).toWideString().c_str() );
+	#else
+	res = _chdir( String::fromUtf8( path.c_str() ).toAnsiString().c_str() );
+	#endif
+#else
+	res = chdir( path.c_str() );
+#endif
+	return -1 != res;
+}
+
+std::string FileSystem::getCurrentWorkingDirectory()
+{
+#ifdef EFSW_COMPILER_MSVC
+	#if defined( UNICODE ) AND !defined( EFSW_NO_WIDECHAR )
+	wchar_t dir[_MAX_PATH];
+	return ( 0 != GetCurrentDirectoryW( _MAX_PATH, dir ) ) ? String( dir )::toUtf8() : std::string();
+	#else
+	char dir[_MAX_PATH];
+	return ( 0 != GetCurrentDirectory( _MAX_PATH, dir ) ) ? String( dir, std::locale() ).toUtf8() : std::string();
+	#endif
+#else
+	char dir[PATH_MAX + 1];
+	getcwd( dir, PATH_MAX + 1 );
+	return std::string( dir );
+#endif
+}
+
 FileInfoMap FileSystem::filesInfoFromPath( const std::string& path )
 {
 	FileInfoMap files;
