d02309bd2178f5536dfa372ec369ecb2e6874dfb
Martín Lucas Golini <spartanj@gmail.com>
2025-11-28 02:27:39 -0300

    Fixing #200, seems to work fine but still needs more testing.

--- premake4.lua
+++ premake4.lua
@@ -8,57 +8,6 @@ efsw_minor_version	= "5"
 efsw_patch_version	= "0"
 efsw_version		= efsw_major_version .. "." .. efsw_minor_version .. "." .. efsw_patch_version
 
-function get_include_paths()
-	local function _insert_include_paths( file )
-		local function _trim(s)
-			return (s:gsub("^%s*(.-)%s*$", "%1"))
-		end
-
-		local paths = { }
-		local lines = file:read('*all')
-
-		for line in string.gmatch(lines, '([^\n]+)')
-		do
-			table.insert( paths, _trim( line ) )
-		end
-
-		file:close()
-
-		return paths
-	end
-
-	local file = io.popen( "echo | gcc -Wp,-v -x c++ - -fsyntax-only 2>&1 | grep -v '#' | grep '/'", 'r' )
-	local include_paths = _insert_include_paths( file )
-
-	if next(include_paths) == nil then
-		file = io.popen( "echo | clang++ -Wp,-v -x c++ - -fsyntax-only 2>&1 | grep -v '#' | grep '/' | grep -v 'nonexistent'", 'r' )
-
-		include_paths = _insert_include_paths( file )
-
-		if next(include_paths) == nil then
-			table.insert( include_paths, "/usr/include" )
-			table.insert( include_paths, "/usr/local/include" )
-		end
-	end
-
-	return include_paths
-end
-
-function inotify_header_exists()
-	local efsw_include_paths = get_include_paths()
-
-	for _,v in pairs( efsw_include_paths )
-	do
-		local cur_path = v .. "/sys/inotify.h"
-
-		if os.isfile( cur_path ) then
-			return true
-		end
-	end
-
-	return false
-end
-
 function string.starts(String,Start)
 	if ( _ACTION ) then
 		return string.sub(String,1,string.len(Start))==Start
@@ -118,10 +67,6 @@ function conf_excludes()
 		excludes { "src/efsw/WatcherInotify.cpp", "src/efsw/WatcherWin32.cpp", "src/efsw/FileWatcherInotify.cpp", "src/efsw/FileWatcherWin32.cpp" }
 	elseif os.is("freebsd") then
 		excludes { "src/efsw/WatcherInotify.cpp", "src/efsw/WatcherWin32.cpp", "src/efsw/WatcherFSEvents.cpp", "src/efsw/FileWatcherInotify.cpp", "src/efsw/FileWatcherWin32.cpp", "src/efsw/FileWatcherFSEvents.cpp" }
-	end
-
-	if os.is("linux") and not inotify_header_exists() then
-		defines { "EFSW_INOTIFY_NOSYS" }
 	end
 end
 
--- src/efsw/FileWatcherInotify.cpp
+++ src/efsw/FileWatcherInotify.cpp
@@ -7,15 +7,10 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/inotify.h>
 #include <sys/select.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
-#ifdef EFSW_INOTIFY_NOSYS
-#include <efsw/inotify-nosys.h>
-#else
-#include <sys/inotify.h>
-#endif
 
 #include <efsw/Debug.hpp>
 #include <efsw/FileSystem.hpp>
@@ -131,6 +126,16 @@ WatchID FileWatcherInotify::addWatch( const std::string& directory, FileWatchLis
 		}
 	}
 
+	// The watch could exists if a file was moved between directories that are being watched
+	// In that case we need to remove the local watch information but *keep* the inotify watch id
+	// open, to be reused with the new watch.
+	{
+		Lock lock( mWatchesLock );
+		auto watchIdExists = mWatches.find( wd );
+		if ( watchIdExists != mWatches.end() )
+			removeWatchLocked( wd, true );
+	}
+
 	efDEBUG( "Added watch %s with id: %d\n", dir.c_str(), wd );
 
 	WatcherInotify* pWatch = new WatcherInotify();
@@ -144,7 +149,7 @@ WatchID FileWatcherInotify::addWatch( const std::string& directory, FileWatchLis
 
 	{
 		Lock lock( mWatchesLock );
-		mWatches.insert( std::make_pair( wd, pWatch ) );
+		mWatches[wd] = pWatch;
 		mWatchesRef[pWatch->Directory] = wd;
 	}
 
@@ -185,7 +190,7 @@ WatchID FileWatcherInotify::addWatch( const std::string& directory, FileWatchLis
 	return wd;
 }
 
-void FileWatcherInotify::removeWatchLocked( WatchID watchid ) {
+void FileWatcherInotify::removeWatchLocked( WatchID watchid, bool skipInotifyRemove ) {
 	WatchMap::iterator iter = mWatches.find( watchid );
 	if ( iter == mWatches.end() )
 		return;
@@ -226,12 +231,14 @@ void FileWatcherInotify::removeWatchLocked( WatchID watchid ) {
 		}
 	}
 
-	int err = inotify_rm_watch( mFD, watchid );
+	if ( !skipInotifyRemove ) {
+		int err = inotify_rm_watch( mFD, watchid );
 
-	if ( err < 0 ) {
-		efDEBUG( "Error removing watch %d: %s\n", watchid, strerror( errno ) );
-	} else {
-		efDEBUG( "Removed watch %s with id: %d\n", watch->Directory.c_str(), watchid );
+		if ( err < 0 ) {
+			efDEBUG( "Error removing watch %d: %s\n", watchid, strerror( errno ) );
+		} else {
+			efDEBUG( "Removed watch %s with id: %d\n", watch->Directory.c_str(), watchid );
+		}
 	}
 
 	efSAFE_DELETE( watch );
@@ -325,7 +332,7 @@ void FileWatcherInotify::run() {
 						if ( curWatcher ) {
 							handleAction( curWatcher, (char*)pevent->name, pevent->mask );
 
-							if ( ( pevent->mask & IN_MOVED_TO ) && curWatcher == currentMoveFrom &&
+							if ( ( pevent->mask & IN_MOVED_TO ) && currentMoveFrom &&
 								 pevent->cookie == currentMoveCookie ) {
 								/// make pair success
 								currentMoveFrom = NULL;
@@ -427,6 +434,12 @@ void FileWatcherInotify::run() {
 				}
 
 				Watcher* watch = it->first;
+
+				// Clear the stale OldFileName.
+				// Since this move is considered complete (moved outside),
+				// the watcher should not be waiting for a pair anymore.
+				watch->OldFileName = "";
+
 				const std::string& oldFileName = it->second;
 
 				/// Check if the file move was a folder already being watched
@@ -534,7 +547,7 @@ void FileWatcherInotify::handleAction( Watcher* watch, const std::string& filena
 											   Actions::Moved, watch->OldFileName );
 		}
 
-		if ( watch->Recursive && FileSystem::isDirectory( fpath ) ) {
+		if ( watch->Recursive && FileSystem::isDirectory( fpath ) && !watch->OldFileName.empty() ) {
 			/// Update the new directory path
 			std::string opath( watch->Directory + watch->OldFileName );
 			FileSystem::dirAddSlashAtEnd( opath );
